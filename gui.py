28c681faad2b094e0f246445ceb1b3a39fe5eac2
tqphu27 <trn.quangph27@gmail.com>
2022-12-05 14:13:52 +0700

    add

---
+++ gui.py
@@ -0,0 +1,268 @@
+import sys, os
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QWidget, QFormLayout, QPushButton, QLineEdit, QInputDialog
+
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+from simulation import MichelsonSimulation
+from visual import showPattern
+
+progname = os.path.basename(sys.argv[0])
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+
+        self.axes = fig.add_subplot(111)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+                                   
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class ScreenCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+
+    def __init__(self, *args, **kwargs):
+        self.is_colored = True
+
+        #initialize a simulation
+        self.simulation = MichelsonSimulation()
+        self.simulation.initialMirrorG([0, 0, 0], [-1, 1, 0])
+        self.simulation.initialMirrorM1([0, 100, 0], [1e-3, -1, 0])
+        self.simulation.initialMirrorM2([100, 0, 0], [-1, 1e-3, 0])
+        self.simulation.insertSource([-20, 0, 0], 635, 20)
+        self.simulation.changeToNonlocal()
+
+        MyMplCanvas.__init__(self, *args, **kwargs)
+
+    def compute_initial_figure(self):
+        showPattern(self.axes, self.simulation, self.is_colored)
+    
+    # def changeLocality(self, text):
+    #     if text=='local' and self.simulation.islocalInterference==False:
+    #         self.simulation.islocalInterference = True
+    #         self.simulation.changeToLocal()
+    #         self.axes.cla()
+    #         showPattern(self.axes, self.simulation, self.is_colored)
+    #         self.draw()
+    #     elif text=='nonlocal' and self.simulation.islocalInterference==True:
+    #         self.simulation.islocalInterference = False
+    #         self.simulation.changeToNonlocal()
+    #         self.axes.cla()
+    #         showPattern(self.axes, self.simulation, self.is_colored)
+    #         self.draw()   
+
+    def changeColor(self, text):
+        if text=='colored' and self.is_colored==False:
+            self.is_colored = True 
+            self.axes.cla()
+            showPattern(self.axes, self.simulation, self.is_colored)
+            self.draw()
+        elif text=='mono' and self.is_colored==True:
+            self.is_colored = False
+            self.axes.cla()
+            showPattern(self.axes, self.simulation, self.is_colored)
+            self.draw()
+
+    def changeSource(self, text):
+        wavelenth = int(text)
+        self.axes.cla()
+        self.simulation.clearSource()
+        self.simulation.insertSource([-20, 0, 0], wavelenth)
+        showPattern(self.axes, self.simulation, self.is_colored)
+        self.draw()
+
+    def changeM1_dir(self, text):
+        y = float(text)
+        self.simulation.setMirrorM1_dir([0,y,0])
+        self.axes.cla()
+        showPattern(self.axes, self.simulation, self.is_colored)
+        self.draw()
+
+    def changeM1(self, text):
+        nx = float(text) # small angle approx.
+        self.simulation.setMirrorM1([nx,-1,0])
+        self.axes.cla()
+        showPattern(self.axes, self.simulation, self.is_colored)
+        self.draw()
+
+    def changeM2_dir(self, text):
+        x = float(text)
+        self.simulation.setMirrorM2_dir([x,0,0])
+        self.axes.cla()
+        showPattern(self.axes, self.simulation, self.is_colored)
+        self.draw()
+
+    def changeM2(self, text):
+        ny = float(text) # small angle approx.
+        self.simulation.setMirrorM2([-1,ny,0])
+        self.axes.cla()
+        showPattern(self.axes, self.simulation, self.is_colored)
+        self.draw()
+
+
+
+class InputdialogDemo(QWidget):
+    def __init__(self,parent=None):
+
+        super(InputdialogDemo,self).__init__(parent)
+        layout=QFormLayout()
+
+        # self.btn_loc=QPushButton("Locality?")
+        # self.btn_loc.clicked.connect(self.getLoc)
+        # self.le_loc=QLineEdit()
+        # self.le_loc.setText('nonlocal')
+        # layout.addRow(self.btn_loc,self.le_loc)
+
+        self.btn_color=QPushButton("Colored or mono. screen?")
+        self.btn_color.clicked.connect(self.getColor)
+        self.le_color=QLineEdit()
+        self.le_color.setText('colored')
+        layout.addRow(self.btn_color,self.le_color)
+
+        self.btn_spec=QPushButton("Wavelenth in nm?")
+        self.btn_spec.clicked.connect(self.getSpec)
+        self.le_spec=QLineEdit()
+        self.le_spec.setText('635')
+        layout.addRow(self.btn_spec,self.le_spec)
+
+        self.btn_M1_dir=QPushButton("M1 location:") 
+        self.btn_M1_dir.clicked.connect(self.getM1_dir)
+        self.le_M1_dir=QLineEdit()
+        self.le_M1_dir.setText('100')
+        layout.addRow(self.btn_M1_dir,self.le_M1_dir)
+        
+        self.btn_M1=QPushButton("M1 angle:")
+        self.btn_M1.clicked.connect(self.getM1)
+        self.le_M1=QLineEdit()
+        self.le_M1.setText('1e-3')
+        layout.addRow(self.btn_M1,self.le_M1)
+        
+        self.btn_M2_dir=QPushButton("M2 location:")
+        self.btn_M2_dir.clicked.connect(self.getM2_dir)
+        self.le_M2_dir=QLineEdit()
+        self.le_M2_dir.setText('100')
+        layout.addRow(self.btn_M2_dir,self.le_M2_dir)
+
+        self.btn_M2=QPushButton("M2 angle:")
+        self.btn_M2.clicked.connect(self.getM2)
+        self.le_M2=QLineEdit()
+        self.le_M2.setText('1e-3')
+        layout.addRow(self.btn_M2,self.le_M2)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Input Dialog")
+
+    # def getLoc(self):
+    #     items = ("local","nonlocal")
+    #     item, ok = QInputDialog.getItem(self,"select Input dialog","Interference type?",items,0,False)
+    #     if ok and item:
+    #         self.le_loc.setText(item)
+
+    def getColor(self):
+        items=("colored","mono")
+        item, ok = QInputDialog.getItem(self,"select Input dialog","Interference type?",items,0,False)
+        if ok and item:
+            self.le_color.setText(item)
+
+    def getM1_dir(self):
+        text, ok = QInputDialog.getText(self,'Text Input Dialog','M1 location?')
+        if ok:
+            self.le_M1_dir.setText(str(text))
+    
+    def getM1(self):
+        text, ok = QInputDialog.getText(self,'Text Input Dialog','M1 angle?')
+        if ok:
+            self.le_M1.setText(str(text))
+
+    def getM2_dir(self):
+        text, ok = QInputDialog.getText(self,'Text Input Dialog','M2 location?')
+        if ok:
+            self.le_M2_dir.setText(str(text))
+        
+    def getM2(self):
+        text, ok = QInputDialog.getText(self,'Text Input Dialog','M2 angle?')
+        if ok:
+            self.le_M2.setText(str(text))
+
+    def getSpec(self):
+        text, ok = QInputDialog.getText(self,"Text Input Dialog","Wavelength?")
+        if ok:
+            self.le_spec.setText(str(text))
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+        sc = ScreenCanvas(self.main_widget, width=5, height=4, dpi=100)
+        l.addWidget(sc)
+
+        idd = InputdialogDemo()
+        l.addWidget(idd)
+
+        # idd.le_loc.textChanged.connect(sc.changeLocality)
+        idd.le_color.textChanged.connect(sc.changeColor)
+        idd.le_spec.textChanged.connect(sc.changeSource)
+        idd.le_M1_dir.textChanged.connect(sc.changeM1_dir)
+        idd.le_M1.textChanged.connect(sc.changeM1)
+        idd.le_M2_dir.textChanged.connect(sc.changeM2_dir)
+        idd.le_M2.textChanged.connect(sc.changeM2)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About",
+                                    """GUI for RealMichelson."""
+                                )
+
+if __name__=='__main__':
+    qApp = QtWidgets.QApplication(sys.argv)
+
+    aw = ApplicationWindow()
+    aw.setWindowTitle("%s" % progname)
+    aw.show()
+    sys.exit(qApp.exec_())
+    #qApp.exec_()
---
+++ visual.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from colour_system import cs_hdtv
+from simulation import spec_wavelengths
+
+
+'''
+    This function calculates RGB map from simulation pattern
+'''
+def RGBConverter(pattern):
+    wavelength_arr = np.array([entry[1] for entry in pattern]).reshape(100,100,-1)[0,0,:]
+    intensity_arr = np.array([entry[2] for entry in pattern]).reshape(100,100,-1)
+    
+    spec_map = np.zeros((100,100,np.size(spec_wavelengths)))
+    
+    for i in range(np.size(wavelength_arr)):
+        idx = int((wavelength_arr[i]-380)/5)
+        if 0<=idx<np.size(spec_wavelengths):
+            spec_map[:,:,idx] += intensity_arr[:,:,i]
+        
+    return np.apply_along_axis(cs_hdtv.spec_to_rgb, axis=2, arr=spec_map)
+
+def showPattern(ax, simulation, is_colored=False):
+    # if simulation.islocalInterference:
+    #     pattern = simulation.localInterference()
+    # else:
+    pattern = simulation.nonlocalInterference()
+    
+    if is_colored:
+        ax.imshow(RGBConverter(pattern), interpolation='none')
+    else:
+        ax.imshow(np.array([entry[2] for entry in pattern]).reshape(100,100,-1).sum(axis=2))
+
